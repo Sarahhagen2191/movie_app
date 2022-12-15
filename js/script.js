88f3f28c66bb4870ca925f3f4d2d554d767dc8cf
Sarah hagen <sarahhagen@sarahs-mbp.home>
2022-12-15 19:53:53 +0000

    completed standard api version

--- js/script.js
+++ js/script.js
@@ -8,19 +8,20 @@ function noMatch() {
 function displayMatches(matches) {
   cardWrapper.innerHTML = '';
 
-  if (!matches.length) {
+  if (!matches) {
     noMatch();
-  }
+  } else{
 
   for (var matchObj of matches) {
     cardWrapper.insertAdjacentHTML('beforeend', `
-    <div class="movie-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${matchObj.movie_image});">
-      <h3>${matchObj.title}</h3>
-      <p>${matchObj.description}</p>
-      <a href="${matchObj.imdb_link}" target="_blank">View More Info Here</a>
+    <div class="movie-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${matchObj.Poster});">
+      <h3>${matchObj.Title}</h3>
+      <p>Release Year: ${matchObj.Year}</p>
+      <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Info Here</a>
     </div>
     `);
   }
+}
 }
 
 function fetchMovies(event) {
@@ -28,18 +29,19 @@ function fetchMovies(event) {
   var searchText = searchInput.value.toLowerCase().trim();
 
   if (keyCode === 13 && searchText) {
-    var matches = [];
 
-    fetch(`https://www.omdbapi.com/?apikey=20dc4c7f&t=${searchText}`).then(function (responseObj) {
-      var dataPromise = responseObj.json();
+    var responsePromise = fetch(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`);
 
-      dataPromise.then(function (data) {
-        console.log(data);
+    function handleResponse(responseObj) {
+      return responseObj.json();
+    }
+
+    responsePromise
+      .then(handleResponse)
+      .then(function (data) {
+        displayMatches(data.Search);
+        searchInput.value = '';
       });
-    });
-
-    searchInput.value = '';
-    displayMatches(matches);
 
 
   }
